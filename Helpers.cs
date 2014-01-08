46b4abb78e76fa33917d1a184bc35621ccd90be9
Wil <wip-@users.noreply.github.com>
2014-01-08 16:43:45 +0900

    Load 101 images, extract curves based on colors, display them

---
+++ Helpers.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace CheckAlphaGradation
+{
+    public static class Helpers
+    {
+        static public int GetBytesPerPixel(PixelFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case PixelFormat.Format8bppIndexed:
+                    return 1;
+
+                case PixelFormat.Format24bppRgb:
+                    return 3;
+
+                case PixelFormat.Format32bppArgb:
+                    return 4;
+
+                case PixelFormat.Format64bppArgb:
+                    return 8;
+
+                default:
+                    Debug.Assert(false);
+                    return 0;
+            }
+        }
+
+        static public bool HasAlphaChannel(PixelFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case PixelFormat.Format8bppIndexed:
+                case PixelFormat.Format24bppRgb:
+                    return false;
+
+                case PixelFormat.Format32bppArgb:
+                case PixelFormat.Format64bppArgb:
+                    return true;
+
+                default:
+                    Debug.Assert(false);
+                    return false;
+            }
+        }
+
+        static public Color GetColorDiff(Color left, Color right)
+        {
+            int a = Math.Abs(left.A - right.A);
+            int r = Math.Abs(left.R - right.R);
+            int g = Math.Abs(left.G - right.G);
+            int b = Math.Abs(left.B - right.B);
+
+            return Color.FromArgb(
+                Convert.ToByte(a.Clamp0_255()),
+                Convert.ToByte(r.Clamp0_255()),
+                Convert.ToByte(g.Clamp0_255()),
+                Convert.ToByte(b.Clamp0_255()));
+        }
+
+        public static int Clamp0_255(this int value)
+        {
+            return value.Clamp(0, 255);
+        }
+
+        public static float Clamp0_255(this float value)
+        {
+            return value.Clamp(0, 255);
+        }
+
+        public static double Clamp0_255(this double value)
+        {
+            return value.Clamp(0, 255);
+        }
+
+        public static int Clamp0_65535(this int value)
+        {
+            return value.Clamp(0, 65535);
+        }
+
+        //http://stackoverflow.com/a/2683487/758666
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+
+
+        public static double ANormalized(this Color color){ return (double)color.A / 255; }
+        public static double RNormalized(this Color color){ return (double)color.R / 255; }
+        public static double GNormalized(this Color color){ return (double)color.G / 255; }
+        public static double BNormalized(this Color color){ return (double)color.B / 255; }
+
+        /// <summary>
+        /// Linearly interpolates over the value x between the points (xMin, yMin) and (xMax, yMax).
+        /// </summary>
+        public static double Lerp(
+            double x,
+            double xMin, double xMax,
+            double yMin, double yMax)
+        {
+            double ratio = (x - xMin) / (xMax - xMin);
+            return yMin + ratio * (yMax - yMin);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+//using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Interop;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace CheckAlphaGradation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IntPtr[] imageBitmaps;
+        IntPtr[] redCurveBitmaps;
+        IntPtr[] greenCurveBitmaps;
+
+        List<double[]> redCurves;
+        List<double[]> greenCurves;
+
+        int colorInfluence = 0;
+
+        private static Bitmap LoadBitmap(String filename)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                if (fs != null)
+                    fs.Close();
+                return null;
+            }
+
+            Bitmap bitmap;
+            bitmap = new Bitmap(fs);
+            return bitmap;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            imageBitmaps = new IntPtr[101];
+            redCurves = new List<double[]>();
+            greenCurves = new List<double[]>();           
+            List<double> redCurvesMins = new List<double>();
+            List<double> greenCurvesMins = new List<double>();
+            List<double> redCurvesMaxs = new List<double>();
+            List<double> greenCurvesMaxs = new List<double>();
+            for (int i = 0; i < 101; ++i)
+            {
+                String filepath = String.Format(
+                    "F:\\myFolder\\alphaGradation_{0:D3}.png", i);
+                Bitmap bitmap = LoadBitmap(filepath);
+                BitmapInfo imageBitmapInfo = new BitmapInfo(bitmap);
+                double[] redCurve = GetColorLevelCurve(imageBitmapInfo,
+                    (color, a) => 
+                    {
+                        double r = color.RNormalized();
+                        if (a == 1) 
+                            a = 0;
+                        //return r /(1-a);
+                        return r;
+                    } 
+                    );
+                double[] greenCurve = GetColorLevelCurve(imageBitmapInfo, 
+                    (color, a) => color.GNormalized()
+                    );
+                imageBitmaps[i] = imageBitmapInfo.ToBitmap().GetHbitmap();
+                redCurves.Add(redCurve);
+                greenCurves.Add(greenCurve);
+                redCurvesMins.Add(redCurve.Min());
+                redCurvesMaxs.Add(redCurve.Max());
+                greenCurvesMins.Add(greenCurve.Min());
+                greenCurvesMaxs.Add(greenCurve.Max());
+            }
+
+            double redCurvesMin = redCurvesMins.Min();
+            double redCurvesMax = redCurvesMaxs.Max();
+            double greenCurvesMin = greenCurvesMins.Min();
+            double greenCurvesMax = greenCurvesMaxs.Max();
+
+
+            redCurveBitmaps = new IntPtr[101];
+            greenCurveBitmaps = new IntPtr[101];
+            for (int i = 0; i < 101; ++i)
+            {
+                BitmapInfo redCurveBitmapInfo = GetColorLevelCurveImage(redCurves[i], redCurvesMin, redCurvesMax);
+                redCurveBitmaps[i] = redCurveBitmapInfo.ToBitmap().GetHbitmap();
+                BitmapInfo greenCurveBitmapInfo = GetColorLevelCurveImage(greenCurves[i], greenCurvesMin, greenCurvesMax);
+                greenCurveBitmaps[i] = greenCurveBitmapInfo.ToBitmap().GetHbitmap();
+            }
+
+            SliderInfluence.Value = 100;
+        }
+
+        public delegate double ColorComponentExtractor(System.Drawing.Color color, double a);
+
+        private double[] GetColorLevelCurve(BitmapInfo source, ColorComponentExtractor extractor)
+        {
+            // vertical Y value where image is sampled
+            // TODO make it a parameter
+            int Ysample = source.Height / 2;
+
+            double[] CurveValues = new double[source.Width];
+            for (int x = 0; x < source.Width; x++)
+            {
+                Color colorSource = source.GetPixelColor(x, Ysample);
+                double alpha = (double)x / (source.Width - 1);
+                double yVal = extractor(colorSource, alpha);
+                CurveValues[x] = yVal;
+            }
+
+            return CurveValues;
+        }
+
+        private BitmapInfo GetColorLevelCurveImage(double[] curveValues, double curvesMin, double curvesMax)
+        {
+            int[] quantizedValues = new int[curveValues.Length];
+            for (int i = 0; i < curveValues.Length; i++)
+            {
+                double scaledValue = Helpers.Lerp(
+                    curveValues[i], curvesMin, curvesMax,
+                    0, 511);
+                quantizedValues[i] = Convert.ToInt32(scaledValue.Clamp(0, 511));
+            }
+
+
+            BitmapInfo dest = new BitmapInfo(512, 512, PixelFormat.Format32bppArgb);
+            for (int x = 0; x < dest.Width; x++)
+            {
+                int yVal = quantizedValues[x];
+
+                // in the original image y is higher at the bottom
+                // we invert y so that output curves are easier to understand
+                for (int y = 0; y < dest.Height; ++y)
+                {
+                    if (y == yVal)
+                        dest.SetPixelColor(x, (dest.Height - 1) - y, Color.Black);
+                    else
+                        dest.SetPixelColor(x, (dest.Height - 1) - y, Color.White);
+                }
+            }
+
+            return dest;
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            double vert = ((ScrollViewer)sender).VerticalOffset;
+            double hori = ((ScrollViewer)sender).HorizontalOffset;
+
+            ScrollViewer[] scrollViewers = new ScrollViewer[]
+            {
+                ScrollViewerImage, 
+                ScrollViewerCurveRed, 
+                ScrollViewerCurveGreen
+            };
+
+            foreach (ScrollViewer scrollViewer in scrollViewers)
+            {
+                if (scrollViewer == null)
+                    continue;
+                scrollViewer.ScrollToVerticalOffset(vert);
+                scrollViewer.ScrollToHorizontalOffset(hori);
+                scrollViewer.UpdateLayout();
+            }
+        }
+
+        private void SliderInfluence_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int newColorInfluence = Convert.ToInt32(Math.Round(SliderInfluence.Value));
+
+            if (newColorInfluence == colorInfluence)
+                return;
+
+            colorInfluence = newColorInfluence;
+            LabelInfluence.Content = newColorInfluence;
+
+            Image.Source =
+                Imaging.CreateBitmapSourceFromHBitmap(
+                    imageBitmaps[colorInfluence], IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+
+            ImageCurveRed.Source =
+                Imaging.CreateBitmapSourceFromHBitmap(
+                    redCurveBitmaps[colorInfluence], IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+
+            ImageCurveGreen.Source =
+                Imaging.CreateBitmapSourceFromHBitmap(
+                    greenCurveBitmaps[colorInfluence], IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+
+
+        private void MyCatch(System.Exception ex)
+        {
+            var st = new StackTrace(ex, true);      // stack trace for the exception with source file information
+            var frame = st.GetFrame(0);             // top stack frame
+            String sourceMsg = String.Format("{0}({1})", frame.GetFileName(), frame.GetFileLineNumber());
+            Console.WriteLine(sourceMsg);
+            MessageBox.Show(ex.Message + Environment.NewLine + sourceMsg);
+            Debugger.Break();
+        }
+
+
+    }
+}
